5a68f6f5fbbabea6e14ea6910b34e0451dd2a331
LaNuggets <150001502+LaNuggets@users.noreply.github.com>
2024-05-23 14:38:36 +0200

    HandleErrorss

--- main.go
+++ main.go
@@ -1,36 +1,73 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"html/template"
+	"log"
 	"net/http"
-	"os"
 )
 
 var port = ":8080"
 
 func main() {
+
 	FileServer := http.FileServer(http.Dir("static"))
+
 	http.Handle("/static/", http.StripPrefix("/static/", FileServer))
 
-	http.HandleFunc("/", home)
+	http.HandleFunc("/home", home)
+	http.HandleFunc("/404", notFound)
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.Redirect(w, r, "/404", http.StatusSeeOther)
+	})
 
 	fmt.Println("Server Start at:")
-	fmt.Println("http://localhost" + port)
+	fmt.Println("http://localhost" + port + "/home")
 
-	error := http.ListenAndServe(port, nil)
-
-	if errors.Is(error, http.ErrServerClosed) {
-		fmt.Println("Server Close")
-	} else if error != nil {
-		fmt.Println("Error starting server: s%", error)
-		os.Exit(1)
+	if err := http.ListenAndServe(port, nil); err != nil {
+		log.Fatal(err)
 	}
 }
 
-func home(w http.ResponseWriter, r *http.Request) { // Lunch a new page for the lose condition
-	tmpl := template.Must(template.ParseFiles("./templates/home.html")) // Read the home page
+func home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/home" {
+		http.Redirect(w, r, "/404", http.StatusSeeOther)
+		return
+	}
 
-	tmpl.Execute(w, nil)
+	if r.Method != "GET" {
+		http.Error(w, "Method is not supported.", http.StatusNotFound)
+		return
+	}
+	tmpl, err := template.ParseFiles("./templates/home.html") // Read the home page
+	if err != nil {
+		log.Printf("\033[31mError parsing template: %v\033[0m", err)
+		http.Error(w, "Internal error, template not found.", http.StatusInternalServerError)
+		return
+	}
+
+	err = tmpl.Execute(w, nil)
+	if err != nil {
+		log.Printf("\033[31mError executing template: %v\033[0m", err)
+		http.Error(w, "Internal error", http.StatusInternalServerError)
+		return
+	}
+}
+
+func notFound(w http.ResponseWriter, r *http.Request) {
+
+	tmpl, err := template.ParseFiles("./templates/notFound.html")
+	if err != nil {
+		log.Printf("\033[31mError parsing template: %v\033[0m", err)
+		http.Error(w, "Internal error, template not found.", http.StatusInternalServerError)
+		return
+	}
+
+	err = tmpl.Execute(w, nil)
+	if err != nil {
+		log.Printf("\033[31mError executing template: %v\033[0m", err)
+		http.Error(w, "Internal error", http.StatusInternalServerError)
+		return
+	}
 }
