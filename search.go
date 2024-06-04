43cf7d141e7079f9b9edd8a5e6f4f3d23a86980e
Jeremy <jeremy.gaudin@ynov.com>
2024-06-04 11:30:40 +0200

    started front end for home page

--- search.go
+++ search.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"ThreadCore/database"
 	"html/template"
 	"log"
 	"net/http"
@@ -20,38 +19,38 @@ func Search(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	search := r.URL.Query().Get("q")
-	media := r.URL.Query().Get("media") // media options  : posts, communities, comments , users
-	sort := r.URL.Query().Get("sort")
-	time := r.URL.Query().Get("time")
+	// search := r.URL.Query().Get("q")
+	// media := r.URL.Query().Get("media") // media options  : posts, communities, comments , users
+	// sort := r.URL.Query().Get("sort")
+	// time := r.URL.Query().Get("time")
 
-	searchedPost := database.GetPostsBySearchString(search)
+	//searchedPost := database.GetPostsBySearchString(search)
 
-	var commentFromSearchedPost []database.Comment
-	for i := 0; i < len(searchedPost); i++ {
-		commentFromSearchedPost = database.GetCommentsByPost(searchedPost[i].Id)
-	}
-	searchedCommunities := database.GetCommunityBySearchString(search)
+	// var commentFromSearchedPost []database.Comment
+	// for i := 0; i < len(searchedPost); i++ {
+	// 	commentFromSearchedPost = database.GetCommentsByPost(searchedPost[i].Id)
+	// }
+	// searchedCommunities := database.GetCommunityBySearchString(search)
 
-	searchPage := struct {
-		Search                  string
-		Media                   string
-		Sort                    string
-		Time                    string
-		SearchedPost            []database.PostDisplay
-		CommentFromSearchedPost []database.Comment
-		SearchedCommunities     []database.CommunityDisplay
-	}{
-		Search:                  search,
-		Media:                   media,
-		Sort:                    sort,
-		Time:                    time,
-		SearchedPost:            searchedPost,
-		CommentFromSearchedPost: commentFromSearchedPost,
-		SearchedCommunities:     searchedCommunities,
-	}
+	// searchPage := struct {
+	// 	Search                  string
+	// 	Media                   string
+	// 	Sort                    string
+	// 	Time                    string
+	// 	SearchedPost            []database.PostInfo
+	// 	CommentFromSearchedPost []database.Comment
+	// 	SearchedCommunities     []database.CommunityInfo
+	// }{
+	// 	Search:                  search,
+	// 	Media:                   media,
+	// 	Sort:                    sort,
+	// 	Time:                    time,
+	// 	SearchedPost:            searchedPost,
+	// 	CommentFromSearchedPost: commentFromSearchedPost,
+	// 	SearchedCommunities:     searchedCommunities,
+	// }
 
-	err = tmpl.Execute(w, searchPage)
+	err = tmpl.Execute(w, nil)
 	if err != nil {
 		log.Printf("\033[31mError executing template: %v\033[0m", err)
 		http.Error(w, "Internal error", http.StatusInternalServerError)
--- static/scripts/javascript.js
+++ static/scripts/javascript.js
@@ -1,26 +1,3 @@
-<<<<<<< HEAD
-// get query value in url from key
-const urlParams = new URLSearchParams(window.location.search);
-
-const searchString = urlParams.get('q');
-if (searchString != null) {
-    document.getElementById("search").value = searchString
-}
-
-
-let search = document.getElementById("search");
-
-search.addEventListener("keypress", function(event) {
-  if (event.key === "Enter") {
-    event.preventDefault();
-    if ('URLSearchParams' in window) {
-        var searchParams = new URLSearchParams(window.location.search);
-        searchParams.set("q", search.value);
-        window.location.search = searchParams.toString();
-    }
-  }
-});
-=======
 // get query value in url from key
 const urlParams = new URLSearchParams(window.location.search);
 
@@ -43,15 +20,14 @@ search.addEventListener("keypress", function(event) {
 });
 
 // If any images are not found, display default image
-const images = document.querySelectorAll('img');
+// const images = document.querySelectorAll('img');
 
-images.forEach(img => {
-  img.addEventListener('error', function handleError() {
-    const defaultImage =
-      'https://bobbyhadz.com/images/blog/javascript-show-div-on-select-option/banner.webp';
+// images.forEach(img => {
+//   img.addEventListener('error', function handleError() {
+//     const defaultImage =
+//       'https://bobbyhadz.com/images/blog/javascript-show-div-on-select-option/banner.webp';
 
-    img.src = defaultImage;
-    img.alt = 'default';
-  });
-});
->>>>>>> main
+//     img.src = defaultImage;
+//     img.alt = 'default';
+//   });
+// });
